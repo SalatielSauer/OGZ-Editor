766e22247803c4ba2abd26c61be6c2f697de6f44
SalatielSauer <tiel_chaves@hotmail.com>
2020-11-19 11:50:19 -0300

    support multiple entities of the same type & fix tabs

--- converters.js
+++ converters.js
@@ -6,34 +6,54 @@ These functions handle the conversion of text to hexadecimal, and also
 from hexadecimal to an array of bytes, later compressed with gzip.
 */
 function floatHex(val){
-    const getHex = i => ('00' + i.toString(16)).slice(-2);
-    var view = new DataView(new ArrayBuffer(4));
-    view.setFloat32(0, val);
-    return Array.apply(null, { length: 4 }).map((_, i) => getHex(view.getUint8(i))).reverse().join('');
+	const getHex = i => ('00' + i.toString(16)).slice(-2);
+	var view = new DataView(new ArrayBuffer(4));
+	view.setFloat32(0, val);
+	return Array.apply(null, { length: 4 }).map((_, i) => getHex(view.getUint8(i))).reverse().join('');
 }
 
 function hexInt(val){
-    const bytes = new Uint8Array(val.toString().split(""));
-    const uint = new Uint32Array(bytes.buffer)[0];
-    return uint;
+	const bytes = new Uint8Array(val.toString().split(""));
+	const uint = new Uint32Array(bytes.buffer)[0];
+	return uint;
 }
 
 function intHex(val, byte){
-    val = parseInt(val);
-    return val.toString(16).padStart(byte, '0').match(/.{2}/g).reverse().join('')
+	val = parseInt(val);
+	return val.toString(16).padStart(byte, '0').match(/.{2}/g).reverse().join('')
 }
 
 function strHex(val){
-    return val.split("").reduce((hex,c)=>hex+=c.charCodeAt(0).toString(16).padStart(2,"0"),"")
+	return val.split("").reduce((hex,c)=>hex+=c.charCodeAt(0).toString(16).padStart(2,"0"),"")
 }
 
 function hexBeautify(val){
-     val = val.replace(/(.{32})/g,"$1\n").replace(/(.{4})/g,"$1 ");
-     return val.replace(/\s\n/g, "\n");
+	val = val.replace(/(.{32})/g,"$1\n").replace(/(.{4})/g,"$1 ");
+	return val.replace(/\s\n/g, "\n");
 }
 
 function hextoByte(val){
-    return new Uint8Array(val.match(/.{2}/g).map(e => parseInt(e, 16)));
+	return new Uint8Array(val.match(/.{2}/g).map(e => parseInt(e, 16)));
+}
+
+function rgbToHex(r, g, b) {
+	return ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+}
+
+/**/
+function isFloat(val) {
+	var floatRegex = /^-?\d+(?:[.,]\d*?)?$/;
+	if (!floatRegex.test(val)) return false;
+	val = parseFloat(val);
+	if (isNaN(val)) return false;
+	return true;
+}
+
+function isInt(val) {
+	var intRegex = /^-?\d+$/;
+	if (!intRegex.test(val)) return false;
+	var intVal = parseInt(val, 10);
+	return parseFloat(val) == intVal && !isNaN(intVal);
 }
 
 /*
@@ -41,16 +61,16 @@ Formatters
 Uses the converters to format the arguments in hexadecimal before concatenating them
 */
 function formathead(worldsize, numents, numpvs, lightmaps, blendmap, numvars){
-    rawheaddata = "4f4354411d00000024000000"
-    try {
-        for (i = 0; i < arguments.length; i++){rawheaddata += intHex(arguments[i], 8)}
-    } finally {return rawheaddata;}
+	rawheaddata = "4f4354411d00000024000000"
+	try {
+		for (i = 0; i < arguments.length; i++){rawheaddata += intHex(arguments[i], 8)}
+	} finally {return rawheaddata;}
 }
 
 // gameident: 03666707300 // not implemented
 
 function formatvar(type, id, val) {
-   return intHex(type, 2) + intHex(id.length, 4) + strHex(id) + intHex(val.length, 4) + strHex(val)
+	return intHex(type, 2) + intHex(id.length, 4) + strHex(id) + intHex(val.length, 4) + strHex(val);
 }
 
 // extras: 00000000 // not implemented
@@ -58,20 +78,21 @@ function formatvar(type, id, val) {
 
 entitiesarray = ["empty", "light", "mapmodel", "playerstart", "envmap", "particles", "sound", "spotlight", "gamespecific"]
 function formatent(xyz, attrs, type) {
-    xyz = xyz.split(" ");
-    attrs = attrs.split(" ");
-    if (entitiesarray.indexOf(type) == -1){type = "empty";};
-    return floatHex(xyz[0]) + floatHex(xyz[1]) + floatHex(xyz[2]) + intHex(attrs[0], 4) + intHex(attrs[1], 4) + intHex(attrs[2], 4) + intHex(attrs[3], 4) + intHex(attrs[4], 4) + intHex(entitiesarray.indexOf(type), 2) + "00";
+	type = entitiesarray.filter(c=> {if (type.match(c)) {return c}})[0]
+	xyz = xyz.split(" ")
+	attrs = attrs.split(" ")
+	if (entitiesarray.indexOf(type) == -1){type = "empty";}
+	return floatHex(xyz[0]) + floatHex(xyz[1]) + floatHex(xyz[2]) + intHex(attrs[0], 4) + intHex(attrs[1], 4) + intHex(attrs[2], 4) + intHex(attrs[3], 4) + intHex(attrs[4], 4) + intHex(entitiesarray.indexOf(type), 2) + "00";
 }
 
 function formatoctree(type, textures, mask){
-    textures = textures.split(" ");
-    textureraw = "";
-    try {
-        for (t = 0; t < textures.length; t++){textureraw += intHex(textures[t], 2) + "00"};
-    } finally {
-        return intHex(type, 2) + textureraw + intHex(mask, 2)
-    }
+	textures = textures.split(" ");
+	textureraw = "";
+	try {
+		for (t = 0; t < textures.length; t++){textureraw += intHex(textures[t], 2) + "00"};
+	} finally {
+		return intHex(type, 2) + textureraw + intHex(mask, 2)
+	}
 }
 
 /* examples
--- main.js
+++ main.js
@@ -1,127 +1,134 @@
 // OGZ Editor by Salatiel
 
-var jsontextarea = document.getElementById("jsondata");
-jsontextarea.value = '{\n"mapsize": 1024,\n"mapvars": {\n\t"skybox": "skyboxes/white",\n\t"maptitle": "Untitled Map by OGZ Editor"\n},\n"entities": {\n\t"particles": {\n\t\t"position": "512 512 512",\n\t\t"attributes": "0 0 0 0xFF00 0"\n\t},\n\t"mapmodel": {\n\t\t"position": "532 532 512",\n\t\t"attributes": "110 50 0 0 0"\n\t}\n}\n}';
-var downloadbtn = document.getElementById("downloadbtn");
-var pako = window.pako;
+var today = new Date()
+var jsontextarea = document.getElementById("jsondata")
+jsontextarea.value = '{\n"mapsize": 1024,\n"mapvars": {\n\t"skybox": "skyboxes/white",\n\t"maptitle": "Untitled Map by OGZ Editor (' + today.getDate() + '/' + (today.getMonth() + 1) + '/' + today.getFullYear() +')"\n},\n"entities": {\n\t"particles": {\n\t\t"position": "512 512 530",\n\t\t"attributes": "0 0 0 0xFFFF 0"\n\t},\n\t"mapmodel": {\n\t\t"position": "512 512 512",\n\t\t"attributes": "180 172 0 0 0"\n\t},\n\t"mapmodel": {\n\t\t"position": "512 512 528",\n\t\t"attributes": "90 177 0 0 0"\n\t}\n}\n}'
+var downloadbtn = document.getElementById("downloadbtn")
+var pako = window.pako
 
 // JSON error catcher
 function setCaretPosition(ctrl, pos) {
 	if (ctrl.setSelectionRange) {
-		ctrl.focus(); ctrl.setSelectionRange(pos, pos);
+		ctrl.focus(); ctrl.setSelectionRange(pos, pos)
 	} else if (ctrl.createTextRange) {
-		var range = ctrl.createTextRange();
-		range.collapse(true);
-		range.moveEnd('character', pos);
-		range.moveStart("character", pos);
-		range.select();
+		var range = ctrl.createTextRange()
+		range.collapse(true)
+		range.moveEnd('character', pos)
+		range.moveStart("character", pos)
+		range.select()
 	}
 }
 
 function pusherrorlbl(state, pos){
-    errorlbl = document.getElementById("errorlbl");
-    if (state == 0){
-        errorlog.style.display = errorlbl.style.display = "none";
-    } else {
-        errorlog.style.display = errorlbl.style.display = "unset";
-    }
-    errorlbl.innerHTML = formattext(jsontextarea.value.slice(0, pos), "#04ff0057", "#00000029") + formattext(jsontextarea.value.slice(pos, pos + 1), "red", "black") + formattext(jsontextarea.value.slice(pos + 1), "#ff000057", "#00000029");
-    errorlbl.scrollTop = jsontextarea.scrollTop;
+	errorlbl = document.getElementById("errorlbl");
+	if (state == 0){
+		errorlog.style.display = errorlbl.style.display = "none"
+	} else {
+		errorlog.style.display = errorlbl.style.display = "unset"
+	}
+	errorlbl.innerHTML = formattext(jsontextarea.value.slice(0, pos), "#04ff0057", "#00000029") + formattext(jsontextarea.value.slice(pos, pos + 1), "red", "black") + formattext(jsontextarea.value.slice(pos + 1), "#ff000057", "#00000029")
+	errorlbl.scrollTop = jsontextarea.scrollTop
 }
 
 function formattext(str, bcolor, tcolor){
-    return bcolor ? "<span style='background-color: " + bcolor + "; color: " + tcolor + "'>" + str + "</span>" : "<span style='color: lightblue;background-color: black;padding-right: 5px;padding-left: 5px;'>" + str + "</span>";
+	return bcolor ? `<span style='background-color: ${bcolor}; color: ${tcolor}'>${str}</span>` : `<span style='color: lightblue; background-color: black; padding-right: 5px; padding-left: 5px;'>${str}</span>`;
 }
 
 function pusherrorlog(er){
 	if (jsontextarea.value[0] != "{") {jsontextarea.value = "{" + jsontextarea.value; readJSON(); return;}
 	if (jsontextarea.value[jsontextarea.value.length - 1] != "}") {jsontextarea.value += "}"; readJSON(); return;}
 
-    er = er.toString().replace("SyntaxError:", formattext("<strong>Syntax Error</strong>:"));
-    er = er.replace("token", "<span style='color: red;'><strong>token</strong></span>");
-    er = er.replace("string", "<span style='color: red;'><strong>string</strong></span>");
+	er = er.toString().replace("SyntaxError:", formattext("<strong>Syntax Error</strong>:"))
+	er = er.replace("token", "<span style='color: red;'><strong>token</strong></span>")
+	er = er.replace("string", "<span style='color: red;'><strong>string</strong></span>")
 
-    errorlog = document.getElementById("errorlog");
-    errorlog.innerHTML = "<p>OGZ Editor report:<br>" + er + "</p>";
-    epos = parseInt(er.substr(er.lastIndexOf("position "), er.length).split(" ")[1]) || 0;
-    setCaretPosition(jsontextarea, epos);
-    pusherrorlbl(1, epos);
-    jsontext = jsontextarea.value;
-	if (jsontext[epos - (epos != 0)] == '"'){errorlog.innerHTML += "<p>Did you mean " + formattext("\",") + " instead of " + formattext("\"" + jsontext[epos]) + "?</p>";};
+	errorlog = document.getElementById("errorlog")
+	errorlog.innerHTML = `<p>OGZ Editor report:<br>${er}</p>`
+	epos = parseInt(er.substr(er.lastIndexOf("position "), er.length).split(" ")[1]) || 0
+	setCaretPosition(jsontextarea, epos)
+	pusherrorlbl(1, epos)
+	jsontext = jsontextarea.value
+	if (jsontext[epos - (epos != 0)] == '"'){errorlog.innerHTML += `<p>Did you mean ${formattext("\",")} instead of ${formattext(jsontext[epos])}?</p>`;}
 	if (jsontext[epos - (epos != 0)].match(/[^a-zA-Z ]+/)){
 		if (jsontext[epos] == ("" || "\n")) {
-			errorlog.innerHTML += "<p>You may need to add a " + formattext("\"") + " somewhere...</p>"
-			errorlbl.innerHTML = formattext(errorlbl.innerText.slice(0, epos), "#04ff0057", "#00000029") + "<span style='color: grey; background-color: #1f252aa6'>  << did you mean " + formattext("\"" + jsontext.substring(jsontext.lastIndexOf("\"", epos) + 1, epos).replace(/[a-zA-Z]|[0-9]+/g, "").replace(/\s/g, "")) + "?</span>\n" + formattext(errorlbl.innerText.slice(epos + 1), "#ff000057", "#00000029");
+			errorlog.innerHTML += `<p>You may need to add a ${formattext("\"")} somewhere...</p>`
+			errorlbl.innerHTML = formattext(errorlbl.innerText.slice(0, epos), "#04ff0057", "#00000029") + "<span style='color: grey; background-color: #1f252aa6'>  << did you mean " + formattext("\"" + jsontext.substring(jsontext.lastIndexOf("\"", epos) + 1, epos).replace(/[a-zA-Z]|[0-9]+/g, "").replace(/\s/g, "")) + "?</span>\n" + formattext(errorlbl.innerText.slice(epos + 1), "#ff000057", "#00000029")
 		} else if (epos != 0) {
-			errorlog.innerHTML += "<p>Maybe you need to remove " + formattext(jsontext[epos]) + " from there...</p>";
+			errorlog.innerHTML += `<p>Maybe you need to remove ${formattext(jsontext[epos])} from there...</p>`
 		}
 	}
 
 	if (! epos) {
 		if (er.includes("Unexpected end of JSON")) {errorlog.innerHTML += "<p>Maybe you forgot to add a bracket there...</p>"}
 		if (errorlbl.innerText[errorlbl.innerText.length - 1] != ("" || "\n")) {errorlbl.innerText += "\n"}
-		errorlbl.innerHTML += "<span style='color: grey'>} << missing bracket(s)?</span>";
+		errorlbl.innerHTML += "<span style='color: grey'>} << missing bracket(s)?</span>"
 	}
-    
-    jsontextarea.focus();
-    
+
+	jsontextarea.focus();
+
 }
 
 // Reads JSON, formats its values and concatenates everything as a raw version of a map file (.ogz)
 function readJSON(){
-    try {
-        JSON.parse(jsontextarea.value);
-    } catch (e) {
-        pusherrorlog(e);
-        return;
-    }
-    jsondata = JSON.parse(jsontextarea.value);
+	try {
+		JSON.parse(jsontextarea.value)
+	} catch (e) {
+		pusherrorlog(e)
+		return;
+	}
+	plainjson = jsontextarea.value;
+	for (e = 0; e < entitiesarray.length; e++) {
+		n = 0
+		enttype = new RegExp(entitiesarray[e], "g")
+		plainjson = plainjson.replace(enttype, et=>{return et + n++})
+	}
+	jsondata = JSON.parse(plainjson)
 
-    mapdata = "";
+	mapdata = ""
 
-    // Header
-    mapdata += formathead(jsondata.mapsize, Object.keys(jsondata.entities).length, 0, 0, 0, Object.keys(jsondata.mapvars).length);
-    
-    // Mapvars
-    for (var key of Object.keys(jsondata.mapvars)) {
-        mapdata += formatvar(2, key, jsondata.mapvars[key])
-    };
+	// Header
+	mapdata += formathead(jsondata.mapsize, Object.keys(jsondata.entities).length, 0, 0, 0, Object.keys(jsondata.mapvars).length)
 
-    // Gameident
-    mapdata += "0366707300"; 
+	// Mapvars
+	for (var key of Object.keys(jsondata.mapvars)) {
+		mapdata += formatvar(2, key, jsondata.mapvars[key])
+	}
 
-    // Extras(?) & texture mru
-    mapdata += "00000000" + "050002000400030005000700";
+	// Gameident
+	mapdata += "0366707300"
 
-    // Entities
-    for (var key of Object.keys(jsondata.entities)) {
-        mapdata += formatent(jsondata.entities[key].position, jsondata.entities[key].attributes, key)
-    };
+	// Extras(?) & texture mru
+	mapdata += "00000000" + "050002000400030005000700"
 
-    // Octree
-    mapdata += formatoctree(2, "2 3 4 5 6 7", 0) + formatoctree(2, "2 3 4 5 6 7", 0) + formatoctree(2, "2 3 4 5 6 7", 0) + formatoctree(2, "2 3 4 5 6 7", 0);
-    mapdata += formatoctree(1, "0 0 0 0 0 0", 0) + formatoctree(1, "0 0 0 0 0 0", 0) + formatoctree(1, "0 0 0 0 0 0", 0) + formatoctree(1, "0 0 0 0 0 0", 0);
+	// Entities
+	for (var key of Object.keys(jsondata.entities)) {
+		mapdata += formatent(jsondata.entities[key].position, jsondata.entities[key].attributes, key)
+	}
 
-    rawtextarea = document.getElementById("mapdata");
-    rawtextarea.value = hexBeautify(mapdata);	// adds spaces in the preview
-    if (rawtextarea.value.length > 0) {downloadbtn.style.display = "unset";}
+	// Octree
+	mapdata += formatoctree(2, "2 3 4 5 6 7", 0) + formatoctree(2, "2 3 4 5 6 7", 0) + formatoctree(2, "2 3 4 5 6 7", 0) + formatoctree(2, "2 3 4 5 6 7", 0)
+	mapdata += formatoctree(1, "0 0 0 0 0 0", 0) + formatoctree(1, "0 0 0 0 0 0", 0) + formatoctree(1, "0 0 0 0 0 0", 0) + formatoctree(1, "0 0 0 0 0 0", 0)
 
-    download(pako.gzip(hextoByte(mapdata)));	// uses pako to compress the byte array as gzip (.ogz)    
+	rawtextarea = document.getElementById("mapdata")
+	rawtextarea.value = hexBeautify(mapdata)	// adds spaces in the preview
+	if (rawtextarea.value.length > 0) {downloadbtn.style.display = "unset";}
+
+	preparedownload(pako.gzip(hextoByte(mapdata)));	// uses pako to compress the byte array as gzip (.ogz)
 }
 
-function download(data){
-    fileName = "mynewmap.ogz";
-    var blob = new Blob([ data ], {
-        type : "application/octet-stream",
-    });
+function preparedownload(data){
+	fileName = "mynewmap.ogz"
+	var blob = new Blob([ data ], {
+		type : "application/octet-stream",
+	});
 
-    if (window.navigator.msSaveBlob) {
-        navigator.msSaveBlob(blob, fileName);
-    } else {
-        var csvUrl = URL.createObjectURL(blob);
-        $("#downloadbtn").attr({
-            "download": fileName,
-            "href": csvUrl
-        })
-    }
+	if (window.navigator.msSaveBlob) {
+		navigator.msSaveBlob(blob, fileName)
+	} else {
+		var csvUrl = URL.createObjectURL(blob);
+		$("#downloadbtn").attr({
+			"download": fileName,
+			"href": csvUrl
+		})
+	}
 }
