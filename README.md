03c5211d98817a344505371f8f28d5af43caecba
SalatielSauer <9287152+SalatielSauer@users.noreply.github.com>
2021-05-04 18:50:19 -0300

    implement octree editing

--- README.md
+++ README.md
@@ -1,26 +1,24 @@
+
 # OGZ-Editor
 A browser tool for writing Sauerbraten .ogz files (maps) using JSON.<br>
 https://salatielsauer.github.io/OGZ-Editor/index.html
 
-#### Using externally
-You can embed **jsogz.js** or **jsogz.min.js** on your own page using the script tag:<br>
-`<script src="https://raw.githubusercontent.com/SalatielSauer/OGZ-Editor/master/jsogz.min.js"></script>`
+#### JSOGZ.js
+You can use **jsogz.js** or **jsogz.min.js** on your own page with the script tag:<br>`<script src="jsogz.min.js"></script>`
 
-This will make the `getOGZ` function available, which receives 2 parameters:
-
-`getOGZ({object}, type)`
+This will make the `JSOGZ` function available, which receives 2 parameters:<br>`JSOGZ({object}, type)`
 - type 0
-	returns hex concatenated as plain text.
+  returns hex concatenated as plain text.
 - type 1
-	returns a byte array containing the hex.
+  returns a byte array containing the hex.
 - type 2
-	returns a byte array containing the .ogz, requires [pako](https://github.com/nodeca/pako).
+  returns a byte array containing the .ogz, requires [pako](https://github.com/nodeca/pako).
 
 The function will try to JSON.parse() the first parameter if you use a string instead of object.
 
-##### Example:
+##### JSOGZ Example:
 ```
-getOGZ(
+JSOGZ(
   {
     "mapsize": 1024,
     "mapvars": {
@@ -40,17 +38,114 @@ getOGZ(
 "4f4354411d000000240000000004000001000000000000000000000000000000010000000208006d61707469746c65060068656c6c6f2103667073000000000005000200040003000500070000000044000000440000004400000000000000ff0000050002020003000400050006000700000202000300040005000600070000020200030004000500060007000002020003000400050006000700000100000000000000000000000000010000000000000000000000000001000000000000000000000000000100000000000000000000000000"
 ```
 
+### Octree Editing
+On a size 10 map (1024x1024) the standard subdivision consists of 8 smaller cubes of size 512x512, and each of them can be subdivided into 8 more cubes.
+
+The octree of a newmap has the following structure:
+```
+"geometry": [
+  "solid",
+  "solid",
+  "solid",
+  "solid",
+  "empty",
+  "empty",
+  "empty",
+  "empty"
+]
+```
+All children are divided into a lower and an upper layer, therefore, these layers consist of 4 cubes each.
+![](https://raw.githubusercontent.com/SalatielSauer/ogz-editor/master/images/octree1.png)
+
+#### Subdivisions
+To add a subdivision to a child, simply create a new list within the previous list:
+```
+"geometry": [
+  "solid",
+  "solid",
+  "solid",
+  "solid",
+  "empty",
+  [
+    "empty",
+    "empty",
+    "solid",
+    "empty",
+    "empty",
+    "empty",
+    "solid",
+    "empty"
+  ],
+  "empty",
+  "empty"
+]
+```
+Each list reduces the size of the cube in half, that is, a subdivision of a 512x512 cube will create 8 new 256x256 cubes within it.
+
+The gif below shows a size 10 map with its first "chunk" subdivided once.
+![](https://raw.githubusercontent.com/SalatielSauer/ogz-editor/master/images/octree2.gif)
+The non-optimized structure (before remip or calclight) would look like:
+```
+"geometry": [
+  [
+    "solid",
+    "solid",
+    "solid",
+    "solid",
+    "solid",
+    "solid",
+    "solid",
+    "solid",
+  ],
+  "solid",
+  "solid",
+  "solid",
+  "empty",
+  "empty",
+  "empty",
+  "empty"
+]
+
+```
+There are some tricks to reduce the number of children, so when executing `/remip` unnecessary subdivisions are removed or converted to pushed faces.
+
+Although Sauer requires the entire Octree filled, even with empty cubes, you don't have to worry about specifying them manually as shown above, **JSOGZ** will fill all the missing space.
+
+#### Textures
+To add textures you can create an object within the list:
+```
+"geometry": [
+  {"solid": {
+    "textures": "1 2 3 4 5 6"
+  }}
+]
+```
+Each number is an index of a registered texture (the textures of the F2 menu ingame). Their positions determine which face of the cube it will be applied to: left right front back bottom top.
+If you want to apply the same texture to all faces, set `allfaces` to 1:
+```
+"geometry": [
+  {"solid": {
+    "allfaces": "1",
+    "textures": "9"
+  }}
+]
+```
+Cubes added after a texture definition will inherit it instead of using the default texture.
+  
+#### Corner Editing
+Detailed cube manipulation is not yet supported.
+
 #### Todo list:
 - [x] Functions to read and convert data from json to hexadecimal
 - [x] Simple json syntax error catcher
 - [x] Light, Mapmodel, Playerstart, Envmap, Particles, Sound and Spotlight entities
 - [x] Support multiple entities of the same type
-- [ ] Teleport, Teledest and pickups entities
+- [ ] Teleport, Teledest and pickup entities
 - [ ] Integer and Float mapvars
-- [ ] Textures
-- [ ] Octree editing
+- [x] Octree editing & Textures
+- [ ] Corner editing
 - [ ] Materials
 - [ ] Lightmaps
-- [ ] Blendmaps (unlikely)
+- [ ] Blendmaps
 - [x] Concatenation and download
 - [x] Conversion of the output to a valid .ogz file directly
--- jsogz.js
+++ jsogz.js
@@ -5,86 +5,153 @@ Converters
 These functions handle the conversion of text to hexadecimal, and also
 from hexadecimal to an array of bytes, later compressed with gzip.
 */
-function floatHex(val){
-	const getHex = i => ('00' + i.toString(16)).slice(-2);
-	var view = new DataView(new ArrayBuffer(4));
-	view.setFloat32(0, val);
-	return Array.apply(null, { length: 4 }).map((_, i) => getHex(view.getUint8(i))).reverse().join('');
+function floatHex(val) {
+	const getHex = i => ("00" + i.toString(16)).slice(-2)
+	var view = new DataView(new ArrayBuffer(4))
+	view.setFloat32(0, val)
+	return Array.apply(null, {length: 4}).map((_, i) => getHex(view.getUint8(i))).reverse().join("")
 }
 
-function intHex(val, byte){
-	val = parseInt(val);
-	return val.toString(16).padStart(byte, '0').match(/.{2}/g).reverse().join('')
+function intHex(val, byte) {
+	val = parseInt(val)
+	return val.toString(16).padStart(byte, "0").match(/.{2}/g).reverse().join("")
 }
 
-function strHex(val){
+function strHex(val) {
 	return val.split("").reduce((hex,c)=>hex+=c.charCodeAt(0).toString(16).padStart(2,"0"),"")
 }
 
-function hextoByte(val){
-	return new Uint8Array(val.match(/.{2}/g).map(e => parseInt(e, 16)));
+function hextoByte(val) {
+	return new Uint8Array(val.match(/.{2}/g).map(e=> parseInt(e, 16)))
+}
+
+function getCubeIndex(x, y, z, gridpower) {
+	let index = 0
+	index += parseInt((x / (1<<gridpower)).toString(2), 8)
+	index += parseInt((y / (1<<gridpower)).toString(2), 8) * 2
+	index += parseInt((z / (1<<gridpower)).toString(2), 8) * 4
+	return index
+}
+
+function getCubePos(index, gridpower) {
+	let octal = index.toString(8)
+	let position = [0, 0, 0]
+	for (var i = 0; i < octal.length; i++) {
+		let digit = parseInt(octal[octal.length - i - 1], 8)
+		position[0] += (Math.floor(digit / 1) % 2) * (2 ** i) * (1<<gridpower)
+		position[1] += (Math.floor(digit / 2) % 2) * (2 ** i) * (1<<gridpower)
+		position[2] += (Math.floor(digit / 4) % 2) * (2 ** i) * (1<<gridpower)
+	}
+	return position
 }
 
 /*
 Formatters
 Uses the converters to format the arguments in hexadecimal before concatenating them
 */
-function formathead(worldsize, numents, numpvs, lightmaps, blendmap, numvars){
-	rawheaddata = "4f4354411d00000024000000"
-	try {
-		for (i = 0; i < arguments.length; i++){rawheaddata += intHex(arguments[i], 8)}
-	} finally {return rawheaddata;}
+function formathead(worldsize, numents, numpvs, lightmaps, blendmap, numvars) {
+	var rawheaddata = "4f4354411d00000024000000"
+	for (var i = 0; i < arguments.length; i++) {
+		rawheaddata += intHex(arguments[i], 8)
+	}
+	return rawheaddata
 }
 
 // gameident: 03666707300 // not implemented
 
 function formatvar(type, id, val) {
-	return intHex(type, 2) + intHex(id.length, 4) + strHex(id) + intHex(val.length, 4) + strHex(val);
+	return intHex(type, 2) + intHex(id.length, 4) + strHex(id) + intHex(val.length, 4) + strHex(val)
 }
 
 // extras: 00000000 // not implemented
 // texture mru: 050002000400030005000700 // not implemented
 
-entitiesarray = ["empty", "light", "mapmodel", "playerstart", "envmap", "particles", "sound", "spotlight", "gamespecific"]
+var entitiesarray = ["entempty", "light", "mapmodel", "playerstart", "envmap", "particles", "sound", "spotlight", "gamespecific"]
 function formatent(xyz, attrs, type) {
-	type = entitiesarray.filter(c=> {if (type.match(c)) {return c}})[0]
-	xyz = xyz.split(" ")
-	attrs = attrs.split(" ")
-	if (entitiesarray.indexOf(type) == -1){type = "empty";}
-	return floatHex(xyz[0]) + floatHex(xyz[1]) + floatHex(xyz[2]) + intHex(attrs[0], 4) + intHex(attrs[1], 4) + intHex(attrs[2], 4) + intHex(attrs[3], 4) + intHex(attrs[4], 4) + intHex(entitiesarray.indexOf(type), 2) + "00";
+	var type = entitiesarray.filter(c=> {if (type.match(c)) {return c}})[0]
+	var xyz = xyz.split(" ")
+	var attrs = attrs.split(" ")
+	if (entitiesarray.indexOf(type) == -1){type = "entempty"}
+	return floatHex(xyz[0]) + floatHex(xyz[1]) + floatHex(xyz[2]) + intHex(attrs[0], 4) + intHex(attrs[1], 4) + intHex(attrs[2], 4) + intHex(attrs[3], 4) + intHex(attrs[4], 4) + intHex(entitiesarray.indexOf(type), 2) + "00"
 }
 
-function formatoctree(type, textures, mask){
-	textures = textures.split(" ");
-	textureraw = "";
-	try {
-		for (t = 0; t < textures.length; t++){textureraw += intHex(textures[t], 2) + "00"};
-	} finally {
-		return intHex(type, 2) + textureraw + intHex(mask, 2)
+var geometryarray = ["children", "empty", "solid", "normal", "lodcube"]
+//var materialsarray = ["air", "water", "lava", "glass", "noclip", "clip", "gameclip", "death", "alpha"]
+var lastTextures = [1, 1, 1, 1, 1, 1]
+function formatoctree(cube) {
+	var cubetype = cube
+	if (typeof cube == "object") {
+		cubetype = Object.keys(cube)[0]
+		cube = cube[Object.keys(cube)[0]]
 	}
+
+	var texturesraw = ""
+	switch(cubetype) {
+		case "children": return "00"; break;
+		case "empty":
+		case "solid":
+		case "normal":
+		case "lodcube":
+			var textures = lastTextures
+			var allfaces = 0
+			if (cube.textures) {
+				lastTextures = textures = cube.textures.split(" ").map(i=>{return parseInt(i)})
+			}
+			if (cube.allfaces) {
+				lastTextures = textures = Array(6).fill(textures[0])
+			}
+			textures.forEach(tex=> {
+				texturesraw += intHex(tex, 4)
+			})
+
+			// default value for "mask" because I still have no idea how it works
+			var mask = 0
+
+			return intHex(geometryarray.indexOf(cubetype), 2) + texturesraw + intHex(mask, 2)
+		break
+	}	
+}
+
+function readOctree(octree, callback) {
+	var data = ""
+	var index = 0
+	function readChildren(child) {
+		child = child.concat(Array(8-child.length).fill("empty"))
+		child.forEach(cube=>{
+			if (Array.isArray(cube)) {
+				data += formatoctree("children")
+				readChildren(cube)
+			} else {
+				data += formatoctree(cube)
+			}
+			index+=1
+		})
+	}
+	readChildren(octree)
+	callback(data)
 }
 
 /* examples
-formathead(1024, 1, 0, 0, 0, 1);
+formathead(1024, 1, 0, 0, 0, 1)
 formatvar(2, "skybox", "ik2k/env/iklake")
 formatent("972 972 516", "336 0 0 0 0", 3)
-formatoctree(2, "2 3 4 5 6 7", 0)
+formatoctree({"solid": {"textures": "1 2 3 4 5 6"}})
 */
 
 function getobjkeys(obj) {return obj ? Object.keys(obj) : []}
 
-function getOGZ(inputdata, outputformat, error) {
+function JSOGZ(inputdata, outputformat, error) {
 	if (typeof inputdata == "string") {
 		for (var e = 0; e < entitiesarray.length; e++) {
 			enttype = new RegExp(entitiesarray[e], "g")
-			inputdata = inputdata.replace(enttype, et=>{return et + e++})
+			inputdata = inputdata.replace(enttype, et=>{return et+e++})
 		}
 		try {
 			inputdata = JSON.parse(inputdata)
 		} catch (e) {if (error) error(e); return}
 	}
 
-	mapdata = ""
+	var mapdata = ""
 
 	// header
 	mapdata += formathead(inputdata.mapsize||1024, getobjkeys(inputdata.entities).length, 0, 0, 0, getobjkeys(inputdata.mapvars).length)
@@ -106,14 +173,16 @@ function getOGZ(inputdata, outputformat, error) {
 	}
 
 	// octree
-	mapdata += formatoctree(2, "2 3 4 5 6 7", 0) + formatoctree(2, "2 3 4 5 6 7", 0) + formatoctree(2, "2 3 4 5 6 7", 0) + formatoctree(2, "2 3 4 5 6 7", 0)
-	mapdata += formatoctree(1, "0 0 0 0 0 0", 0) + formatoctree(1, "0 0 0 0 0 0", 0) + formatoctree(1, "0 0 0 0 0 0", 0) + formatoctree(1, "0 0 0 0 0 0", 0)
+	var geometry = inputdata.geometry ? inputdata.geometry : []
+	readOctree(geometry, octree=>{
+		mapdata += octree
+	})
 
 	switch(outputformat) {
 		case 0: default: return mapdata; break; 
 		case 1: return hextoByte(mapdata); break;
 		case 2: 
-			if (pako) return pako.gzip(hextoByte(mapdata));
+			if (window.pako) return window.pako.gzip(hextoByte(mapdata))
 			console.log("%c pako not found, you need it to get the data in .ogz format: https://github.com/nodeca/pako", "color: red")
 		break;
 	}
