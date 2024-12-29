e6f64063c50edbf238ad5921da4c725d62060b9b
Salatiel <9287152+SalatielSauer@users.noreply.github.com>
2024-12-29 10:54:16 -0300

    fix ogzeditor_texrotation preset

--- scripts/examples/ogzeditor_textrotation.js
+++ scripts/examples/ogzeditor_textrotation.js
@@ -14,7 +14,7 @@ geometry(()=>{
 	let color = 0;
 	for (let i = 0; i < 19; i++) {
 		// texts can be rotated on 3 axes: yaw, pitch and roll.
-		cubes.push(ogzeditor.text(`^f${color}----^f~OGZ Editor----`, 512+(i*5), 512-(i*5), 600-i, 1462, 1, 0, i*10, i*10))
+		cubes.push(ogzeditor.text(`^f${color}----OGZ Editor----`, 512+(i*5), 512-(i*5), 600-i, 1462, 1, 0, i*10, i*10))
 		color = color == 8 ? 0 : color+1;
 	}
 
